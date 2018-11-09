7726db06a79f874f3bf0cd0f7978f6811c949e08
EL <elwiszixuan@hotmail.com>
2018-11-09 17:03:58 +0800

    react render index cards

--- reactDemo/html/js/reactDemo.js
+++ reactDemo/html/js/reactDemo.js
@@ -1,23 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import '../../bootstrap-4.0.0-dist/css/bootstrap.css';
-import '../css/general.css'
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import '../../bootstrap-4.0.0-dist/css/bootstrap.css';
+// import '../css/general.css'
 
-function Cube(props) {
+function Card(props) {
     return (
-        <div class="col-xs-4 align-self-start ">
-            <div class="card bg-secondary text-light">
-                <div class="card-body">
-                    <h6 class="card-title">props.title</h6>
-                    <span><i class="fa fa-bar-chart fa-lg" aria-hidden="true"></i></span>
-                    <h4 class="card-text">完成数值 props.index</h4>
+        <div className="col-xs-4 align-self-start ">
+            <div className="card bg-secondary text-light">
+                <div className="card-body">
+                    <h6 className="card-title">{props.title}</h6>
+                    <span><i className="fa fa-bar-chart fa-lg" aria-hidden="true"></i></span>
+                    <h4 className="card-text">完成数值 {props.result}</h4>
                 </div>
             </div>
         </div>
     );
 }
 
-class CubeTab extends React.Component{
+
+
+class CardsRow extends React.Component{
+    
+    testArray(){
+        let kpiArray = [];
+        for(let i =0;i<16;i++){
+            let kpi ={
+                title:i+"",
+                result:i
+            }
+            kpiArray[i]=kpi
+        }
+       
+        return (kpiArray);
+    }
+
+
+    render(){
+        
+        let kpiArray = this.testArray();
+        return(
+            
+        );
+    }
+
+}
+
+
+class CardsTab extends React.Component{
     constructor(props){
         super(props);
         this.state={
@@ -25,12 +54,47 @@ class CubeTab extends React.Component{
         }
     }
 
-    renderCube(i){
+    renderCube(title,result){
         return(
-            <Cube
-                title = {'考核项目'+i}
-                index = {i}
+            <Card
+                title = {title}
+                result = {result}
             />
         );
     }
+
+
+
+    render(){
+        return(
+            <div>
+                <div className="row justify-content-between" style={{background:'aquamarine'}}>
+                    {this.renderCube('考核项目',11)}
+                    {this.renderCube('考核项目',22)}
+                    {this.renderCube('考核项目',33)}
+                    {this.renderCube('考核项目',44)}
+                    {this.renderCube('考核项目',55)}
+                </div>
+                <div className="row justify-content-between" style={{background:'aquamarine'}}>
+                    {this.renderCube('考核项目',66)}
+                    {this.renderCube('考核项目',77)}
+                    {this.renderCube('考核项目',88)}
+                    {this.renderCube('考核项目',99)}
+                    {this.renderCube('考核项目',1010)}
+                </div>
+                <div className="row justify-content-between" style={{background:'aquamarine'}}>
+                    {this.renderCube('考核项目',1111)}
+                    {this.renderCube('考核项目',1212)}
+                    {this.renderCube('考核项目',1313)}
+                    {this.renderCube('考核项目',1414)}
+                    {this.renderCube('考核项目',1515)}
+                </div>
+            </div>
+        )
+    }
 }
+
+ReactDOM.render(
+    <CardsTab />,
+    document.getElementById('testR')
+);
