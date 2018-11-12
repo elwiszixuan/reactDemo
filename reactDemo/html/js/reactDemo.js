95d16daf552900ba6c416efa418e4819dacf6f56
EL <elwiszixuan@hotmail.com>
2018-11-12 18:07:20 +0800

    react+echarts demo

--- reactDemo/html/js/reactDemo.js
+++ reactDemo/html/js/reactDemo.js
@@ -3,6 +3,8 @@
 // import '../../bootstrap-4.0.0-dist/css/bootstrap.css';
 // import '../css/general.css'
 
+
+//单个kpi指标项自定义组件
 function Card(props) {
     return (
 
@@ -19,11 +21,7 @@ function Card(props) {
     );
 }
 
-function TabDropDown(props) {
-    return (<p onClick={props.onClick}>-----------------↓-----------------</p>)
-
-}
-
+//指标项展示框 --动态读取后台考核项目
 class CardsTab extends React.Component {
     constructor(props) {
         super(props);
@@ -92,16 +90,63 @@ class CardsTab extends React.Component {
 
 }
 
+//下拉自定义组件
+function TabDropDown(props) {
+    return (<p onClick={props.onClick}>-----------------↓-----------------</p>)
+
+}
+
+//echart 组件
+class EchartComponent extends React.Component{
+
+}
+//环比
+function LinkRelativeRatio(props){
+     // 基于准备好的dom，初始化echarts实例
+     var myChart = echarts.init(document.getElementById('main'));
+                    
+     // 指定图表的配置项和数据
+     var option = {
+         // title: {
+         //     text: 'ECharts 入门示例'
+         // },
+         tooltip: {},
+         legend: {
+             data:['销量']
+         },
+         xAxis: {
+             data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+         },
+         yAxis: {},
+         series: [{
+             name: '销量',
+             type: 'bar',
+             data: [5, 20, 36, 10, 10, 20]
+         }]
+     };
+
+     // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option);
+
+    render(){
+        return(
+            <div></div>
+        );
+    }
+    
+}
 
 
 
 //=================================================================
 ReactDOM.render(
     <CardsTab />,
-    document.getElementById('testR')
+    document.getElementById('testR'),
+    // <LinkRelativeRatio/>,
+    // document.getElementById('main')
 );
 
-
+//调试用数组
 function testArray() {
     let kpiArray = [];
     for (let i = 0; i < 16; i++) {
@@ -115,6 +160,7 @@ function testArray() {
     return kpiArray;
 }
 
+//将传入的数组，分组切片，分发到指标展示框的每一行
 function convertArray(arrayFromBack) {
 
     var list = [];
